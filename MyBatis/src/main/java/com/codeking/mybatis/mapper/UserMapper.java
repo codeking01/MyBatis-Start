15733ec726df8442af08b24cf55ddcae1e627adc
codeking <jialiang_xiong@163.com>
2022-11-24 14:45:14 +0800

    2022.11.24,master commit, 完成增删改查，测试了各种获取参数的方式，#{}，${},
    Signed-off-by: codeking <1094234526@qq.com>

--- MyBatis/src/main/java/com/codeking/mybatis/mapper/UserMapper.java
+++ MyBatis/src/main/java/com/codeking/mybatis/mapper/UserMapper.java
@@ -1,9 +1,24 @@
 package com.codeking.mybatis.mapper;
 
+import com.codeking.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import sun.plugin.javascript.navig.Link;
+
+import java.util.ArrayList;
+
 /**
  * @author : codeking
  * @Create : 2022/11/23 15:55
  */
 public interface UserMapper {
-    int insertUser();
+    int insertUser(String username, String password, Integer age, char sex, String email);
+
+    int insertUserByEntity(User user);
+
+    int deleteUserById(@Param("id") Integer id);
+
+    int updateUserById(@Param("username") String username,@Param("password") String password,@Param("age") Integer age,@Param("sex") char sex,@Param("email") String email,@Param("id") Integer id);
+
+    User getById(@Param("id") Integer id);
+    ArrayList<User> getAllUser();
 }
--- MyBatis/src/main/java/com/codeking/mybatis/mapper/testMapper.java
+++ MyBatis/src/main/java/com/codeking/mybatis/mapper/testMapper.java
@@ -1,10 +1,18 @@
 package com.codeking.mybatis.mapper;
 
+import com.codeking.mybatis.pojo.test;
+import sun.plugin.javascript.navig.Link;
+
+import java.util.ArrayList;
+
 /**
  * @author : codeking
  * @create : 2022/11/23 16:18
  */
 public interface testMapper {
     int addTest();
-    void deleteById();
+    int deleteById();
+    int updateTest();
+    ArrayList<test> getAll();
+    test getSingleTest();
 }
